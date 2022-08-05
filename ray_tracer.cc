6eca9c04af7fb9aaee6706248c0bee29827c852d
charlie-zjj <jjzhao314@gmail.com>
2022-08-05 16:09:10 +0800

    perlin noise

--- ray_tracer.cc
+++ ray_tracer.cc
@@ -61,6 +61,18 @@ hittable_list two_spheres() {
   return objects;
 }
 
+hittable_list two_perlin_spheres() {
+  hittable_list objects;
+
+  auto pertext = make_shared<noise_texture>(4);
+  objects.add(make_shared<sphere>(point3(0, -1000.0, 0), 1000,
+                                  make_shared<lambertian>(pertext)));
+  objects.add(make_shared<sphere>(point3(0, 2, 0), 2,
+                                  make_shared<lambertian>(pertext)));
+
+  return objects;
+}
+
 hittable_list random_scene() {
   hittable_list world;
 
@@ -131,10 +143,16 @@ int main() {
     vfov = 20.0;
     aperture = 0.1;
     break;
-
-  default:
   case 2:
     world = two_spheres();
+    lookfrom = point3(13, 2, 3);
+    lookat = point3(0, 0, 0);
+    vfov = 20.0;
+    break;
+
+  default:
+  case 3:
+    world = two_perlin_spheres();
     lookfrom = point3(13, 2, 3);
     lookat = point3(0, 0, 0);
     vfov = 20.0;
