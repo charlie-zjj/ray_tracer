52205dc6e5b611edbfbf80cd0827887754de42bf
charlie-zjj <jjzhao314@gmail.com>
2022-08-05 16:26:56 +0800

    image texture

---
+++ rtw_stb_image.h
@@ -0,0 +1,13 @@
+/**
+ * @file rtw_stb_image.h
+ * @author charlie
+ * @date 2022-08-05
+ */
+
+#ifndef __RTW_STB_IMAGE_H_
+#define __RTW_STB_IMAGE_H_
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "external/stb_image.h"
+
+#endif
